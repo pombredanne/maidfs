6a322faef5fc2bb24e023ee2e26f217b1a234acc
Sumin Byeon <suminb@gmail.com>
2013-11-15 04:08:12 -0700

    Python enum.

--- maidsim/filetype.py
+++ maidsim/filetype.py
@@ -1,12 +1,20 @@
-class FileType:
-    '''
-    Enum containing all the file types we are considering.
-    '''
+# class FileType:
+#     '''
+#     Enum containing all the file types we are considering.
+#     '''
 
 
-    TEXT = 0
-    BINARY = 1
-    IMAGE = 2
-    VIDEO = 3
+#     TEXT = 0
+#     BINARY = 1
+#     IMAGE = 2
+#     VIDEO = 3
 
-    # TODO: add more types as necessary
+#     # TODO: add more types as necessary
+
+#
+# How about this?
+#
+def enum(**enums):
+    return type('Enum', (), enums)
+
+FileType = enum(TEXT=0, BINARY=1, IMAGE=2, VIDEO=3, AUDIO=4)
