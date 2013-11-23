f5819596a59d6a16dcba72912f2600add5f90fa5
Sumin Byeon <suminb@gmail.com>
2013-11-23 04:12:30 -0700

    Calculating compression speed, compression ratio, and decompression speed.

--- aux/compression_exp.py
+++ aux/compression_exp.py
@@ -5,19 +5,20 @@ import subprocess
 import logging
 
 COMP_FILE_NAME = '/tmp/compressed'
-DECOMP_FILE_NAME = '/tmp/decompressed'
+DECOMP_FILE_NAME = '/tmp/decompressed/'
 
 COMPRESSION_TYPES = (
     # command, suffix
-    ('gzip', 'gz'),
+    ('tar', 'tar.gz'),
+    ('tar', 'tar.bz2'),
     ('7z', '7z'),
 )
 
 # FIXME: This is for testing purposes only. Will replace with an environment
 # value or something configurable.
 FILES = (
-    '/Users/sumin/Downloads/dump.sql',
-    '/Users/sumin/Downloads/rfc4269.txt',
+    '/Users/sumin/Documents/compression-test/text',
+    '/Users/sumin/Documents/compression-test/video',
 )
 
 NULL_FILE = open(os.devnull, 'w')
@@ -44,34 +45,51 @@ class Profiler:
 
 
 def get_file_size(file_name):
-    stats = os.stat(file_name)
-    return stats.st_size
+    if os.path.isfile(file_name):
+        stats = os.stat(file_name)
+        return stats.st_size
+
+    elif os.path.isdir(file_name):
+        # http://stackoverflow.com/questions/1392413/calculating-a-directory-size-using-python
+        return sum([os.path.getsize(f)
+            for f in os.listdir(file_name) if os.path.isfile(f)])
+
+    else:
+        raise Exception('Unsupported file type')
 
 
 def build_comp_cmd(comp_util, file_name, suffix):
-    if comp_util == 'gzip':
-        return [comp_util, '-c', file_name], \
-            open('{}.{}'.format(COMP_FILE_NAME, suffix), 'w')
+    if suffix == 'tar.gz':
+        return [comp_util, '-zcf', '{}.{}'.format(COMP_FILE_NAME, suffix),
+            file_name], NULL_FILE
 
-    elif comp_util == '7z':
+    elif suffix == 'tar.bz2':
+        return [comp_util, '-jcf', '{}.{}'.format(COMP_FILE_NAME, suffix),
+            file_name], NULL_FILE
+
+    elif suffix == '7z':
         return [comp_util, 'a', '{}.{}'.format(COMP_FILE_NAME, suffix),
             file_name], NULL_FILE
 
     else:
-        raise Exception('Unsupported compression type: {}'.format(comp_type))
+        raise Exception('Unsupported compression type: {}'.format(suffix))
 
 
 def build_decomp_cmd(comp_util, file_name, suffix):
-    if comp_util == 'gzip':
-        return [comp_util, '-df', '{}.{}'.format(COMP_FILE_NAME, suffix)], \
-            open('{}.{}'.format(DECOMP_FILE_NAME, suffix), 'w')
+    if suffix == 'tar.gz':
+        return [comp_util, '-zxf', '{}.{}'.format(COMP_FILE_NAME, suffix),
+            '-C', DECOMP_FILE_NAME], NULL_FILE
 
-    elif comp_util == '7z':
+    elif suffix == 'tar.bz2':
+        return [comp_util, '-jxf', '{}.{}'.format(COMP_FILE_NAME, suffix),
+            '-C', DECOMP_FILE_NAME], NULL_FILE
+
+    elif suffix == '7z':
         return [comp_util, 'x', '{}.{}'.format(COMP_FILE_NAME, suffix),
             '-o/tmp/', '-aoa'], NULL_FILE
 
     else:
-        raise Exception('Unsupported compression type: {}'.format(comp_type))
+        raise Exception('Unsupported compression type: {}'.format(suffix))
 
 
 def time_cmd(cmd=[], fout=NULL_FILE):
@@ -107,11 +125,21 @@ def measure(file_name, comp_type):
 
     decomp_time = time_cmd(decomp_cmd, fout)
 
-    print uncomp_file_size, comp_time, comp_file_size, decomp_time
+    return uncomp_file_size, comp_time, comp_file_size, decomp_time
+
+
+def print_as_csv(*args):
+    uncomp_file_size, comp_time, comp_file_size, decomp_time = args
+
+    print ','.join(map(str, [
+        uncomp_file_size / comp_time, # compression speed
+        comp_file_size / float(uncomp_file_size), # compression ratio
+        uncomp_file_size / decomp_time, # decompression speed
+    ]))
 
 
 if __name__ == '__main__':
 
     for file_name in FILES:
         for comp_type in COMPRESSION_TYPES:
-            measure(file_name, comp_type)
+                print_as_csv(*measure(file_name, comp_type))
