4fae06ee46f94ad16cc057fa045cd723aa315f80
Sumin Byeon <suminb@gmail.com>
2013-12-11 15:47:42 -0700

    Add snappy algorithm.

--- maidsim/compressionalgorithm.py
+++ maidsim/compressionalgorithm.py
@@ -44,6 +44,11 @@ lzop_alg = CompressionAlgorithm("lzop",
         [0.613333536, 1.000015532, 0.99447258, 0.99498836, 0.988155026],
         [268974410.2, 1089490417, 848173708.1, 791794805.8, 832463808.8])
 
+snappy_alg = CompressionAlgorithm("snappy",
+        [191299557.2, 608377338.1, 570671029, 592835681.5, 520625377.6],
+        [0.684816788, 1.000110463, 0.995022754, 0.99184873, 0.993852803],
+        [442635713.1, 966817800.6, 1096636902, 1011958109, 1016081001])
+
 # Some hypothetical algorithms
 faster1_alg = CompressionAlgorithm("faster1",
         map(lambda x: x*1.5, gzip_alg.compression_speed),
